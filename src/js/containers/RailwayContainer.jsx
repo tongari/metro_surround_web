54a86fc822a57c00b1f26330f791faf8d1caed65
tongari <info@tongari-as.com>
2016-10-31 18:56:29 +0900

    test

--- src/js/containers/RailwayContainer.jsx
+++ src/js/containers/RailwayContainer.jsx
@@ -8,8 +8,11 @@ import railwayConfig from '../config/railway';
 
 let dragStart = 0;
 let dragEnd = 0;
-const setNavChangeSlide = (railwayId) => {
+const setNavChangeSlide = (elm_, railwayId) => {
+  const elm = elm_;
+  console.log(railwayId);
   dragEnd = -1 * railwayId * window.innerWidth;
+  // elm.style.transform = `translateX(${dragEnd}px)`;
 };
 
 /**
@@ -19,37 +22,36 @@ const setNavChangeSlide = (railwayId) => {
 const slide = () => (
   {
     transform: `translateX(${dragEnd}px)`,
+    border: '1px solid',
   }
 );
 
-const addEvent = (onChangeRailwayId_) => {
+const addEvent = (elm_, onChangeRailwayId_) => {
+  const elm = elm_;
   const onChangeRailwayId = onChangeRailwayId_;
-  return (elm_) => {
-    const elm = elm_;
 
-    elm.addEventListener('touchstart', (e) => {
-      dragStart = e.touches[0].clientX;
-    });
+  elm.addEventListener('touchstart', (e) => {
+    dragStart = e.touches[0].clientX;
+  });
 
-    elm.addEventListener('touchmove', (e) => {
-      const currentX = e.touches[0].clientX;
-      const calc = dragEnd + (currentX - dragStart);
-      elm.style.transform = `translateX(${calc}px)`;
-    });
+  elm.addEventListener('touchmove', (e) => {
+    const currentX = e.touches[0].clientX;
+    const calc = dragEnd + (currentX - dragStart);
+    elm.style.transform = `translateX(${calc}px)`;
+  });
 
-    elm.addEventListener('touchend', (e) => {
-      const currentX = e.changedTouches[0].clientX;
-      const tmp = dragEnd + (currentX - dragStart);
-      const slideIndex = Math.round(tmp / window.innerWidth);
-      dragEnd = slideIndex * window.innerWidth;
-      const maxId = railwayConfig.length - 1;
-      const lastPos = -1 * maxId * window.innerWidth;
-      if (dragEnd > 0) dragEnd = 0;
-      if (dragEnd < lastPos) dragEnd = lastPos;
-      elm.style.transform = `translateX(${dragEnd}px)`;
-      onChangeRailwayId(Math.abs(slideIndex));
-    });
-  };
+  elm.addEventListener('touchend', (e) => {
+    const currentX = e.changedTouches[0].clientX;
+    const tmp = dragEnd + (currentX - dragStart);
+    const slideIndex = Math.round(tmp / window.innerWidth);
+    dragEnd = slideIndex * window.innerWidth;
+    const maxId = railwayConfig.length - 1;
+    const lastPos = -1 * maxId * window.innerWidth;
+    if (dragEnd > 0) dragEnd = 0;
+    if (dragEnd < lastPos) dragEnd = lastPos;
+    elm.style.transform = `translateX(${dragEnd}px)`;
+    onChangeRailwayId(Math.abs(slideIndex));
+  });
 };
 
 /**
@@ -58,6 +60,50 @@ const addEvent = (onChangeRailwayId_) => {
  * @returns {XML}
  * @constructor
  */
+
+
+class RailwayContainer extends React.Component {
+  componentDidMount() {
+    console.log('componentDidMount');
+    const { store, bActions } = this.props;
+    setNavChangeSlide(this.sliderNode, store.getRailwayId);
+    addEvent(this.sliderNode, bActions.onChangeRailwayId);
+  }
+
+  componentWillReceiveProps() {
+    const { store } = this.props;
+    setNavChangeSlide(this.sliderNode, store.getRailwayId);
+    this.render();
+  }
+
+  render() {
+    console.log('render!!!!');
+    const { store } = this.props;
+
+    return (
+      <div className={railwayCss.container}>
+        <div
+          className={railwayCss.slider}
+          style={slide()}
+          ref={node => (this.sliderNode = node)}
+        >
+          {
+            railwayConfig.map((elm, index) => (
+              <Railway
+                key={index}
+                index={index}
+                current={store.getRailwayId}
+                apiData={store.apiData}
+              />
+            ))
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+/*
 const RailwayContainer = (props) => {
   const {
     store,
@@ -65,13 +111,14 @@ const RailwayContainer = (props) => {
   } = props;
 
   setNavChangeSlide(store.getRailwayId);
-  // ref={addEvent(bActions.onChangeRailwayId)}
+
 
   return (
     <div className={railwayCss.container}>
       <div
         className={railwayCss.slider}
         style={slide()}
+        ref={addEvent(bActions.onChangeRailwayId)}
       >
         {
           railwayConfig.map((elm, index) => (
@@ -87,6 +134,7 @@ const RailwayContainer = (props) => {
     </div>
   );
 };
+*/
 
 const mapStateToProps = state => ({
   store: state,
