c09b73bbe2b321d73370fac642aa112fdaeb64a6
tongari <info@tongari-as.com>
2016-11-25 21:30:34 +0900

    bg color bug fix

--- src/js/containers/Root.jsx
+++ src/js/containers/Root.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import RailwayContainer from './RailwayContainer';
 import GlobalMenuContainer from './GlobalMenuContainer';
+import { bodyBg } from '../utils/view';
 
 const Root = (props) => {
   const {
     store,
   } = props;
 
-  // bodyBg(store.railwayId.current);
+  bodyBg(store.railwayId.current);
 
   return (
     <div>
